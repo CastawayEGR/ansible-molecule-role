f84801be508ce6d84c962d00d7d546315be6be5a
Michael Tipton <mike@ibeta.org>
2018-11-23 18:04:44 -0500

    update molecule test file

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,13 +6,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-@pytest.mark.parametrize('pkg', ['docker', 'python-docker-py'])
+@pytest.mark.parametrize('pkg', ['docker-ce'])
 def test_package(host, pkg):
     package = host.package(pkg)
     assert package.is_installed
 
 
-@pytest.mark.parametrize('svc', ['docker'])
+@pytest.mark.parametrize('svc', ['docker-ce'])
 def test_service(host, svc):
     service = host.service(svc)
     assert service.is_enabled
